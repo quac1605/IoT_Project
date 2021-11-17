38266188d20930bb206c5f90edb2924ebd4e1c27
Minh Hoang Tran <hoangtranminh651997@gmail.com>
2021-11-17 11:44:18 +0100

    .

--- Server/Cam_Flask.py
+++ Server/Cam_Flask.py
@@ -16,7 +16,7 @@ def gen():
        rval, frame = vc.read() 
        cv2.imwrite('pic.jpg', frame) 
        yield (b'--frame\r\n' 
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n') 
 @app.route('/video_feed') 
 def video_feed(): 
    """Video streaming route. Put this in the src attribute of an img tag.""" 
