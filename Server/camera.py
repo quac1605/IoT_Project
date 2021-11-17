9ead3b7f3ef56e182d871124df13ba11fb3bc977
Minh Hoang Tran <hoangtranminh651997@gmail.com>
2021-11-17 12:12:45 +0100

    .

--- Server/camera.py
+++ Server/camera.py
@@ -5,7 +5,7 @@ import time
 import numpy as np
 
 class VideoCamera(object):
-    def __init__(self, flip = False):
+    def __init__(self, resolution=(1024, 768), framerate=60,flip = False):
         self.vs = PiVideoStream().start()
         self.flip = flip
         time.sleep(2.0)
