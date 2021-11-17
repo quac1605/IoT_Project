81ace8a62d261fb42ad6c8cadc257ec09cfa0133
Minh Hoang Tran <hoangtranminh651997@gmail.com>
2021-11-17 11:52:26 +0100

    .

--- Server/Cam_Flask.py
+++ Server/Cam_Flask.py
@@ -1,26 +1,30 @@
-# import required modules test
-from flask import Flask, render_template, Response 
-import picamera 
-import cv2
-import socket 
-import io 
-app=Flask(__name__,template_folder='templates') 
-@app.route('/') 
-def index(): 
-   """Video streaming .""" 
-   return render_template('index.html') 
-def gen(): 
-   """Video streaming generator function.""" 
-   vc = cv2.VideoCapture(0)
-   while True: 
-       rval, frame = vc.read() 
-       cv2.imwrite('pic.jpg', frame) 
-       yield (b'--frame\r\n' 
-              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n') 
-@app.route('/video_feed') 
-def video_feed(): 
-   """Video streaming route. Put this in the src attribute of an img tag.""" 
-   return Response(gen(), 
-                   mimetype='multipart/x-mixed-replace; boundary=frame') 
-if __name__ == '__main__': 
-	app.run(host='0.0.0.0', debug=True, threaded=True) 
+from flask import Flask, render_template, Response, request
+from camera import VideoCamera
+import time
+import threading
+import os
+
+pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
+
+# App Globals (do not edit)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html') #you can customze index.html here
+
+def gen(camera):
+    #get camera frame
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', debug=False)
