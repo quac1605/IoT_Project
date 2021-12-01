494a208b28f705bdb982970c42939bc35315b15b
Minh Hoang Tran <hoangtranminh651997@gmail.com>
2021-12-01 11:11:01 +0100

    .

--- Server/Cam_Flask.py
+++ Server/Cam_Flask.py
@@ -24,7 +24,7 @@ def index():
 
 def gen(camera):
     # get camera frame
-    while True:
+    #while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
