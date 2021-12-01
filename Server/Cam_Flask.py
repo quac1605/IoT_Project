93bf87c5118ef8e65450e9c4fe4a6c44eee1f1c1
Minh Hoang Tran <hoangtranminh651997@gmail.com>
2021-12-01 10:45:12 +0100

    .

--- Server/Cam_Flask.py
+++ Server/Cam_Flask.py
@@ -1,29 +1,52 @@
+from time import sleep
+import KeyBoardRecogniton as kw
+import Control as ctrl
 from flask import Flask, render_template, Response, request
 from camera import VideoCamera
 import time
 import threading
 import os
 
-pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
+sys.path.insert(0, "//home//pi//Desktop//CAR//Modul//Motor_Control")
+speed = 0
+angle = 0
+
+pi_camera = VideoCamera(flip=False)  # flip pi camera if upside down.
 
 # App Globals (do not edit)
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
+
 
 def gen(camera):
-    #get camera frame
+    # get camera frame
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(pi_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# try to control throw keyboard behavior
+
+
+@app.route('/online_control', methods=['POST'])
+def online_control():
+    if request.method == 'POST':
+        speed = request.form['speed']
+        angle = request.form['angle']
+        #ctrl.speed(speed)
+        #ctrl.grad(angle)
+        print("speed: ", speed)
+        print("angle: ", angle)
 
 if __name__ == '__main__':
 
