9c7fdf0ef097f0e7cc84582205cee1933d42566c
Minh Hoang Tran <hoangtranminh651997@gmail.com>
2021-12-01 12:02:43 +0100

    .

--- Server/Cam_Flask.py
+++ Server/Cam_Flask.py
@@ -36,14 +36,27 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # try to control throw keyboard behavior
-
+"""
 @app.route("/online_control/<speed>/<angle>")
 def online_control(speed,angle):
         #ctrl.speed(speed)
         #ctrl.grad(angle)
-        print(int(speed))
-        print(int(angle))
+        print(speed)
+        print(angle)
+        return 0
+"""
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
 
 if __name__ == '__main__':
 
