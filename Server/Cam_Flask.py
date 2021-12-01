a5f2451d596c6528239b0510f6bd3c1f96950041
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-01 15:02:37 +0100

    .

--- Server/Cam_Flask.py
+++ Server/Cam_Flask.py
@@ -50,20 +50,15 @@ def online_control():
 
 """
 
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
 
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/online_control', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         user = request.form['nm']
         print(user)
-        return redirect(url_for('success', name=user))
     else:
         user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
 
 
 if __name__ == '__main__':
