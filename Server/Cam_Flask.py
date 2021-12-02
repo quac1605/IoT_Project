a7f1448eac7f96ce02f2a4b7f4edeb0c182f812b
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-02 14:59:23 +0100

    .

--- Server/Cam_Flask.py
+++ Server/Cam_Flask.py
@@ -12,13 +12,19 @@ import threading
 sys.path.insert(0, "//home//pi//Desktop//IoT_Project//Modul//Motor_Control")
 import Control as ctrl
 
-
+"""
 speed_queue = Queue()
 angle_queue = Queue()
+"""
+speed = 0
+angle = 0
+
 pi_camera = VideoCamera(flip=False)  # flip pi camera if upside down.
 
 # App Globals (do not edit)
+sio = socketio.Server(async_mode='threading')
 app = Flask(__name__)
+app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 
 
 @app.route('/')
@@ -42,9 +48,9 @@ def video_feed():
 
 # try to control throw keyboard behavior
 def control_loop():
+    global speed
+    global angle
     while True:
-        global speed;
-        global angle;
         ctrl.speed(int(speed))
         ctrl.grad(int(angle))
         print("speed:  ", speed)
@@ -53,15 +59,17 @@ def control_loop():
 
 def thread1(threadname, q1,q2):
     #read variable "a" modify by thread 2
+    global speed
+    global angle
     while True:
-        speed = q1.get()
-        angle = q2.get()
+        #speed = q1.get()
+        #angle = q2.get()
         if speed is None: return # Poison pill
-        ctrl.speed(int(speed))
-        #print("speed in control_thread",speed)
+        #ctrl.speed(int(speed))
+        print("speed in control_thread",speed)
         if angle is None: return
-        ctrl.grad(int(angle))
-        #print("angle in control_thread",angle)
+        #ctrl.grad(int(angle))
+        print("angle in control_thread",angle)
         sleep(0.08)
 
 thread1 = Thread( target=thread1, args=("Thread-1", speed_queue,angle_queue) )
@@ -69,10 +77,12 @@ thread1 = Thread( target=thread1, args=("Thread-1", speed_queue,angle_queue) )
 
 @app.route('/online_control', methods=['POST'])
 def online_control():
+    global speed
+    global angle
     speed = request.form['speed']
     angle = request.form['angle']
-    speed_queue.put( speed)
-    angle_queue.put( angle);
+    #speed_queue.put( speed)
+    #angle_queue.put( angle);
     #print("online control speed received: ",speed)
     #print("online control angle received",angle)
     return ("",204)
