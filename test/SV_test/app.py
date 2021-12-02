40366f97381cf86aed2463b4f4af367d4444d00f
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-02 19:15:06 +0100

    Jquery + socketio

---
+++ test/SV_test/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+values = {
+    'speed': 0,
+    'angle': 0,
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html',**values)
+
+@socketio.on('connect')
+def test_connect():
+    emit('after connect',  {'data':'Lets dance'})
+
+@socketio.on('Value changed')
+def value_changed(message):
+    values[message['who']] = message['data']
+    emit('update value', message, broadcast=True)
+    print(message['data'])
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0',debug=False)
