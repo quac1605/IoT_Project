3a936d3677a9a7d64b230a136133199e9cab9ae6
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-02 18:30:58 +0100

    Jquery + socketio

--- Server/Cam_Flask.py
+++ Server/Cam_Flask.py
@@ -34,7 +34,7 @@ eventlet.monkey_patch()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',**values)
 
 
 def gen(camera):
@@ -121,5 +121,5 @@ def online_control():
 if __name__ == '__main__':
     #_thread.start_new_thread(control_loop, (speed,angle))
     #thread1.start()
-    socketio.run(app, host='0.0.0.0')
+    socketio.run(app, host='0.0.0.0', debug=True)
     #thread1.join()
