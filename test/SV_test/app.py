ec32db44528e152c02faf6e8f2344a380009fd85
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-04 10:29:16 +0100

    .

--- test/SV_test/app.py
+++ test/SV_test/app.py
@@ -24,4 +24,4 @@ def value_changed(message):
     print(message['data'])
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0',debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000)
