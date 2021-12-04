fe32c300ace92ade7da03333424552e1eb9cebff
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-04 12:27:26 +0100

    .

--- test/SV_test/app.py
+++ test/SV_test/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from camera import VideoCamera
+
+
+pi_camera = VideoCamera(flip=False)  # flip pi camera if upside down.
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -12,6 +16,18 @@ values = {
 @app.route('/')
 def index():
     return render_template('index.html',**values)
+
+def gen(camera):
+    # get camera frame
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @socketio.on('connect')
 def test_connect():
