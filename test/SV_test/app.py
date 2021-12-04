701d5dbe4a920c07b7300b8c1258fac0a5ab6ee3
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-04 12:47:14 +0100

    .

--- test/SV_test/app.py
+++ test/SV_test/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request
 from flask_socketio import SocketIO, emit
+from threading import Lock
 
 from camera import VideoCamera
 pi_camera = VideoCamera(flip=False)  # flip pi camera if upside down.
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+
+from videoStream import videoStreamBp
+app.register_blueprint(videoStreamBp)
+
 
 values = {
     'speed': 0,
@@ -15,18 +21,6 @@ values = {
 @app.route('/')
 def index():
     return render_template('index.html',**values)
-
-def gen(camera):
-    # get camera frame
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(pi_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @socketio.on('connect')
 def test_connect():
---
+++ test/SV_test/videoStream.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, Response
+videoStreamBp = Blueprint('video_stream', __name__)
+
+# Raspberry Pi camera module (requires picamera package)
+from camera_pi import Camera
+def gen(camera):
+    # Video streaming generator function.
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@videoStreamBp.route('/videopi')
+def video_stream():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
