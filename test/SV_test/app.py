495e131ee867baedcd0b3ce0236c4cc731575e9d
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-04 11:17:39 +0100

    .

--- test/SV_test/app.py
+++ test/SV_test/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SERVER_NAME'] = 'iotcar.cc'
 socketio = SocketIO(app)
 
 values = {
