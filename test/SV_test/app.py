d7c60fa18cfca52d18e3fb28430ee7aed231d2a0
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-04 21:10:58 +0100

    .

--- test/SV_test/app.py
+++ test/SV_test/app.py
@@ -55,4 +55,4 @@ def value_changed(message):
     print(message['data'])
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
