39dcca8bd29dd2178cbcaa75f5ee1dd30a02b323
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-04 13:30:02 +0100

    .

--- test/SV_test/app.py
+++ test/SV_test/app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template, Response, request
 from flask_socketio import SocketIO, emit
 from threading import Lock
 import cv2
-from imutils.video.pivideostream import PiVideoStream
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
@@ -16,27 +21,23 @@ values = {
     'angle': 0,
 }
 
-camera = PiVideoStream().start()  # use 0 for web camera
-#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
-# for local webcam use cv2.VideoCapture(0)
+vc = cv2.VideoCapture(0)
 
-def gen_frames():  # generate frame by frame from camera
+def gen():
+    """Video streaming generator function."""
     while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+        rval, frame = vc.read()
+        cv2.imwrite('t.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.route('/')
 def index():
