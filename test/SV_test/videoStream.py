0dd899119d8900e4a5adfcc90383060ab18a749a
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-04 13:05:53 +0100

    .

--- test/SV_test/videoStream.py
+++ test/SV_test/videoStream.py
@@ -1,17 +1,22 @@
-from flask import Blueprint, render_template, Response
+from flask import Blueprint, Flask, render_template, Response
+
+import cv2
+
 videoStreamBp = Blueprint('video_stream', __name__)
 
+camera = cv2.VideoCapture(0)
 # Raspberry Pi camera module (requires picamera package)
-from camera_pi import VideoCamera
-pi_camera = VideoCamera(flip=False)
-def gen(camera):
-    # Video streaming generator function.
+def gen_frames():  
     while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-@videoStreamBp.route('/videopi')
-def video_stream():
-    return Response(gen(pi_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@videoStreamBp.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
