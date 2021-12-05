bb660ea1cbe44551a387463a7b2c033e0ae9f5b7
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-05 09:05:50 +0100

    .

--- test/SV_test/app.py
+++ test/SV_test/app.py
@@ -22,12 +22,13 @@ def index():
   
 @socketio.on('connect', namespace='/control')
 def test_connect():
-    emit('after connect',  {'data':'Lets dance'})
+    print('client connected')
+    emit('after connect',  {'data':'Lets dance'}, namespace='/control')
 
 @socketio.on('Value changed', namespace='/control')
 def value_changed(message):
     values[message['who']] = message['data']
-    emit('update value', message, broadcast=True)
+    emit('update value', message, broadcast=True, namespace='/control')
     print(message['data'])
 
 
