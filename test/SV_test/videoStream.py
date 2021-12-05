785a81a30b7dba3b7c5b42aa0ed5795b4851a5fd
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2021-12-05 10:06:35 +0100

    .

--- test/SV_test/videoStream.py
+++ test/SV_test/videoStream.py
@@ -5,16 +5,14 @@ import cv2
 videoStreamBp = Blueprint('video_feed', __name__)
 
 from camera_pi import VideoCamera
-
 # Raspberry Pi camera module (requires picamera package)
-def gen_frames():  
+def gen_frames(camera):  
     # get camera frame
     while True:
-        pi_camera = VideoCamera(flip=False)
-        frame = pi_camera.get_frame()
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @videoStreamBp.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(VideoCamera(flip=False)), mimetype='multipart/x-mixed-replace; boundary=frame')
