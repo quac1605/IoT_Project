37b09255f5ee101a53722d6db1fca9c3c0e6f22a
Minh Hoang Tran <hoangtranminh651997@gmail.com>
2021-12-15 13:04:56 +0100

    final

--- Server/videoStream.py
+++ Server/videoStream.py
@@ -21,10 +21,10 @@ def gen_frames(camera):
     # Su dung OpenCV cua Khanh o day de return ra angle
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-       '''
-       yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + open('video_image.jpg', 'rb').read() + b'\r\n\r\n')
-        '''
+       
+#       yield (b'--frame\r\n'
+#               b'Content-Type: image/jpeg\r\n\r\n' + open('video_image.jpg', 'rb').read() + b'\r\n\r\n')
+    
                
 
 @videoStreamBp.route('/video_feed')
