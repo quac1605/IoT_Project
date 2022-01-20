a9b561bcfe13e8a46697ad69aa6234afcf2be569
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2022-01-20 16:33:52 +0100

    add 1 line and configuaration for camera_pi

--- Server/camera_pi.py
+++ Server/camera_pi.py
@@ -15,7 +15,6 @@ auto_values = {
 old_value = 0
 class VideoCamera(object):
     global auto_values
-    global old_value
     def __init__(self, resolution=(480,320), framerate=120,flip = False):
         self.vs = PiVideoStream().start()
         self.flip = flip
@@ -34,7 +33,7 @@ class VideoCamera(object):
         frame = self.flip_if_needed(self.vs.read())
         ret, jpeg = cv2.imencode('.jpg', frame)
         combine_value = detect_lane(frame)
-        print('combine_value ', combine_value['angle'])
+        print('combine_value ', combine_value['angle'], 'old_value', old_value)
         auto_values['speed'] = combine_value['speed']
         if ((combine_value['angle'] - old_value >= 10) and auto_values['angle'] <= 100):
             auto_values['angle'] = auto_values['angle'] + 25;
