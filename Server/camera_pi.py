eeb4c691e6bc674e1a2c300ebb5b5c833ab44d43
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2022-01-21 11:38:27 +0100

    add 1 line and configuaration for camera_pi

--- Server/camera_pi.py
+++ Server/camera_pi.py
@@ -36,10 +36,18 @@ class VideoCamera(object):
         combine_value = detect_lane(frame)
         print('combine_value ', combine_value['angle'], 'old_value', old_value)
         auto_values['speed'] = combine_value['speed']
+        '''
         if ((combine_value['angle'] - old_value >= 12) and auto_values['angle'] <= 100):
             auto_values['angle'] = auto_values['angle'] + 20;
         elif ((combine_value['angle'] - old_value <= -12) and auto_values['angle'] >= -100):
             auto_values['angle'] = auto_values['angle'] - 20;
+        '''
+        if ((combine_value['angle'] >= 1) and auto_values['angle'] <= 100):
+            auto_values['angle'] = auto_values['angle'] + 5
+        elif ((combine_value['angle'] - old_value <= 1) and auto_values['angle'] >= -100):
+            auto_values['angle'] = auto_values['angle'] - 5
+        else:
+            auto_values['angle'] = 0
         old_value = combine_value['angle']
         
         
