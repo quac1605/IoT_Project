fbfd98d72637f42ee9621d5089407e7e97b2110a
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2022-01-22 18:35:23 +0100

    add 1 line and configuaration for camera_pi

--- OPEN_CV/Detection_Edges.py
+++ OPEN_CV/Detection_Edges.py
@@ -8,16 +8,16 @@ def detection_edges(frame):
 
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-
-
+	#blue range
+	lower_white = np.array([105, 140, 12])
+	upper_white = np.array([164, 255, 107])
+	#mask erstellen
+	mask = cv2.inRange(hsv,lower_white,upper_white)
+	'''
 	#define white range
 	lower_white = np.array([0, 0, 140])
 	upper_white = np.array([179, 50, 255])
-
-	#mask erstellen
-	mask = cv2.inRange(hsv,lower_white,upper_white)
-
-
+	'''
 	#detection edges of lines
 	edges = cv2.Canny(mask, 200, 400)
 
