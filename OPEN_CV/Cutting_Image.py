32da348c119a244953c1a63f239be83c61e580d1
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2022-01-23 09:52:38 +0100

    add 1 line and configuaration for camera_pi

--- OPEN_CV/Cutting_Image.py
+++ OPEN_CV/Cutting_Image.py
@@ -9,8 +9,8 @@ def cutting_image(edges):
 	polygon = np.array([[
 		(0,height * 0.55),
 		(width,height * 0.55),
-		(width,height * 0.75),
-		(0,height * 0.75),
+		(width,height * 0.85),
+		(0,height * 0.85),
 		]],np.int32)
 
 	cv2.fillPoly(mask, pts = [polygon], color=(255,255,255))
