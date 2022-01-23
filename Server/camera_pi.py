99a090fcd3999abd8aca7835c129405a99bcc540
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2022-01-23 09:51:02 +0100

    add 1 line and configuaration for camera_pi

--- Server/camera_pi.py
+++ Server/camera_pi.py
@@ -48,9 +48,9 @@ class VideoCamera(object):
             elif ((combine_value['angle'] - (auto_values['angle']/3) <= -5) and auto_values['angle'] >= -100):
                 auto_values['angle'] = auto_values['angle'] - 10
         elif(combine_value['lane_number'] == 1):
-            if ((combine_value['angle'] >= 45) and auto_values['angle'] <= 100):
+            if ((combine_value['angle'] >= 50) and auto_values['angle'] <= 100):
                 auto_values['angle'] = auto_values['angle'] + 10
-            elif ((combine_value['angle'] <= -45) and auto_values['angle'] >= -100):
+            elif ((combine_value['angle'] <= -50) and auto_values['angle'] >= -100):
                 auto_values['angle'] = auto_values['angle'] - 10
             elif ((auto_values['angle'] >= 0)):
                 auto_values['angle'] = auto_values['angle'] - 5
