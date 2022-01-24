d9ac990fe01061e56fa8424b091115b13f1d1d26
unknown <nhat.q.nguyen@student.fh-kiel.de>
2022-01-24 20:50:07 +0100

    final

--- Server/camera_pi.py
+++ Server/camera_pi.py
@@ -38,13 +38,13 @@ class VideoCamera(object):
         auto_values['speed'] = combine_value['speed']
 
         if(combine_value['lane_number'] == 2):
-            if (combine_value['angle'] > 60 and (combine_value['angle'] - (auto_values['angle']/1.2) >= 8) and auto_values['angle'] <= 100):
+            if (combine_value['angle'] > 55 and (combine_value['angle'] - (auto_values['angle']/1.2) >= 8) and auto_values['angle'] <= 100):
                 auto_values['angle'] = auto_values['angle'] + 4
-            elif (combine_value['angle'] < -60 and (combine_value['angle'] - (auto_values['angle']/1.2) <= -8) and auto_values['angle'] >= -100):
+            elif (combine_value['angle'] < -55 and (combine_value['angle'] - (auto_values['angle']/1.2) <= -8) and auto_values['angle'] >= -100):
                 auto_values['angle'] = auto_values['angle'] - 4
-            elif ((combine_value['angle']*1.1 - (auto_values['angle']) >= 2) and auto_values['angle'] <= 100):
+            elif ((combine_value['angle']*1.15 - (auto_values['angle']) >= 2) and auto_values['angle'] <= 100):
                 auto_values['angle'] = auto_values['angle'] + 4
-            elif ((combine_value['angle']*1.1 - (auto_values['angle']) <= -2) and auto_values['angle'] >= -100):
+            elif ((combine_value['angle']*1.15 - (auto_values['angle']) <= -2) and auto_values['angle'] >= -100):
                 auto_values['angle'] = auto_values['angle'] - 4
         elif(combine_value['lane_number'] == 1):
             if ((combine_value['angle'] >= 75) and auto_values['angle'] <= 100):
