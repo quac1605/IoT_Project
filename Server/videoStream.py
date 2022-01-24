5381c6a4b63bbb5aecb3675ea0552f513408966a
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2022-01-24 11:36:53 +0100

    final

--- Server/videoStream.py
+++ Server/videoStream.py
@@ -32,10 +32,10 @@ def gen_frames():
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
                
-@videoStreamBp.route('/video_feed')
-def video_feed():
-    return Response(gen_frames_edges(pi_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 @videoStreamBp.route('/video_edges_feed')
 def video_edges_feed():
+    return Response(gen_frames_edges(pi_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@videoStreamBp.route('/video_feed')
+def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
