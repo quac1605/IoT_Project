e26932c738f344a588156c9101da0d952eda1337
unknown <nhat.q.nguyen@student.fh-kiel.de>
2022-01-24 16:05:06 +0100

    final

--- Server/camera_pi.py
+++ Server/camera_pi.py
@@ -38,9 +38,9 @@ class VideoCamera(object):
         auto_values['speed'] = combine_value['speed']
 
         if(combine_value['lane_number'] == 2):
-            if (combine_value['angle'] > 20 and (combine_value['angle'] - (auto_values['angle']/4) >= 10) and auto_values['angle'] <= 100):
+            if (combine_value['angle'] > 35 and (combine_value['angle'] - (auto_values['angle']/4) >= 10) and auto_values['angle'] <= 100):
                 auto_values['angle'] = auto_values['angle'] + 10
-            elif (combine_value['angle'] < -20 and (combine_value['angle'] - (auto_values['angle']/4) <= -10) and auto_values['angle'] >= -100):
+            elif (combine_value['angle'] < -35 and (combine_value['angle'] - (auto_values['angle']/4) <= -10) and auto_values['angle'] >= -100):
                 auto_values['angle'] = auto_values['angle'] - 10
             elif ((combine_value['angle'] - (auto_values['angle']) >= 5) and auto_values['angle'] <= 100):
                 auto_values['angle'] = auto_values['angle'] + 5
