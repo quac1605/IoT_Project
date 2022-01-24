45c5f03af509ab5a020f21de05af57d1e4bff332
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2022-01-24 09:46:47 +0100

    final

--- Server/app.py
+++ Server/app.py
@@ -62,7 +62,7 @@ def thread1(threadname, val):
             ctrl.speed(int(auto_values['speed']))
             ctrl.grad(int(auto_values['angle']))
             socketio.emit('Sever updated value', { 'who': 'speed', 'data': auto_values['speed'] }, broadcast=True, namespace='/control')
-            ocketio.emit('Sever updated value', { 'who': 'angle', 'data': auto_values['angle'] }, broadcast=True, namespace='/control')
+            socketio.emit('Sever updated value', { 'who': 'angle', 'data': auto_values['angle'] }, broadcast=True, namespace='/control')
             #print('auto set angle = ',auto_values['angle'], 'auto set speed = ', auto_values['speed'])
         #code for manuell
         elif (control_values['mode'] == 'manuell'):
