9e8f5f1c9f93eadde452cface48aa6b12df103f0
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2022-01-24 11:14:26 +0100

    final

--- Server/videoStream.py
+++ Server/videoStream.py
@@ -23,8 +23,18 @@ def gen_frames(camera):
     # Su dung OpenCV cua Khanh o day de return ra angle
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + open('video_image.jpg', 'rb').read() + b'\r\n\r\n')
-  
+
+def gen_frames_edges():  
+    # get camera frame and public to global
+    while True:
+    # Su dung OpenCV cua Khanh o day de return ra angle
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + open('video_image_edges.jpg', 'rb').read() + b'\r\n\r\n')
                
 @videoStreamBp.route('/video_feed')
 def video_feed():
     return Response(gen_frames(pi_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@videoStreamBp.route('/video_edges_feed')
+def video_edges_feed():
+    return Response(gen_frames_edges(), mimetype='multipart/x-mixed-replace; boundary=frame')
