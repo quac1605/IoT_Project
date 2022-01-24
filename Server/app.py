8b4929118e6d3101b51ad4cc3e4850e1b39b87cb
minh.h.tran <minh.h.tran@student.fh-kiel.de>
2022-01-24 09:34:12 +0100

    final

--- Server/app.py
+++ Server/app.py
@@ -28,7 +28,7 @@ from camera_pi import auto_values
 from videoStream import videoStreamBp
 app.register_blueprint(videoStreamBp)
 
-from edgesStream import edgesStreamBp
+#from edgesStream import edgesStreamBp
 #app.register_blueprint(edgesStreamBp)
 
 #Create  GUI for namespace "/"
