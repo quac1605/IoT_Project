58b0bea974b93224360be854e0b374ce4e167a9b
unknown <nhat.q.nguyen@student.fh-kiel.de>
2022-01-25 11:24:46 +0100

    final

--- Server/camera_pi.py
+++ Server/camera_pi.py
@@ -88,7 +88,7 @@ class VideoCamera(object):
             old_laneline = 1
         elif(combine_value['lane_number'] == 0):
             if ((check_side == 0) and (auto_values['angle'] > 35) or (auto_values['angle'] < -35)):
-                auto_values['speed'] = 33
+                auto_values['speed'] = 60
                 auto_values['angle'] = -(auto_values['angle'] / abs(auto_values['angle']))*50
                 check_side = check_side + 1
             else:
